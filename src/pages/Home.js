47eed2e6eb3d8bd1e6d5141e4127c2e9a4159294
SkyRex06 <shivamprasad5953@gmail.com>
2025-02-21 16:02:10 +0530

    Building the Home Page

---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Home = () => {
+    return <div className="HomePageWrapper">
+        <div className="FormWrapper">
+            <img src = "/code-sync.png" alt = "code-sync-logo"/>
+            <h4 className="MainLabel">Paste Invitation ROOM ID</h4>
+            <div className="inputGroup">
+                <input 
+                     type="test"
+                     className="inputBox"
+                     placeholder="ROOM ID"
+                     />
+                <input 
+                     type="test"
+                     className="inputBox"
+                     placeholder="USERNAME"
+                     />  
+                <button className="btn joinbtn" >Join</button>
+                <span className = "createInfo">
+                    If you don't have an invite then create &nbsp;
+                    <a href = "" className="createNewntn">
+                        New Room
+                    </a>
+                </span>
+
+            </div>
+        </div>
+        <footer>
+            <h4>Built by Team BlockSmiths</h4>
+        </footer>
+    </div>;
+    
+};
+ 
+export default Home;
