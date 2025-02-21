d29d6e2571277946fedfaaf27d8918a61c59a387
SkyRex06 <shivamprasad5953@gmail.com>
2025-02-21 18:39:47 +0530

    building editor page interface side panel

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Client from "../components/Client";
 
 const EditorPage = () => {
-        return <div>Editor Page</div>
+
+        const [clients,setClients]= useState([
+                {socketId:1 , USERNAME:'Shivam'},
+                {socketId:2 , USERNAME:'Manav'}
+        
+        
+        ]);
+
+        return <div className="mainWrap">
+        <div className="aside">
+                <div className="asideInner">
+                        <div className="logo">
+                                <img className="logoImage" src ="/code-sync.png" alt="logo"/>
+                        </div>
+                        <h3>Connected</h3>
+                        <div className="clientsList" >
+                                {
+                                        clients.map((clients) => (
+                                                <Client key={clients.socketId} USERNAME={clients.USERNAME}/>
+                                        ))}
+                        </div>
+                </div>
+        
+        </div>
+        <div className="editorWrap">Editor Goes here...</div>
+        </div>
 };
 
 export default EditorPage;
