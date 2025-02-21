497e4acc88bdc79d116b652016816c29bed86670
SkyRex06 <shivamprasad5953@gmail.com>
2025-02-21 20:52:54 +0530

    added the editor for javascript

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import { useEffect } from "react";
+import "codemirror/lib/codemirror.css"; // Import core CSS
+import "codemirror/mode/javascript/javascript"; // Import JavaScript mode
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+import "codemirror/theme/dracula.css";
+import CodeMirror from "codemirror";
 
 const Editor = () => {
+    useEffect(() => {
+        const textarea = document.getElementById("realtimeEditor");
 
-    return (
-        <div>editor goes her....</div>
-            
-    )  
+        
+        if (textarea.classList.contains("cm-initialized")) return;
+
+        textarea.classList.add("cm-initialized");
+
+        CodeMirror.fromTextArea(textarea, {
+            mode: { name: "javascript", json: true },
+            theme: "dracula",
+            autoCloseTags: true,
+            autoCloseBrackets: true,
+            lineNumbers: true,
+        });
+
+    }, []); 
+
+    return <textarea id="realtimeEditor"></textarea>;
 };
 
 export default Editor;
+
+
